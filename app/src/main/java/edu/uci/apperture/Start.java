81f6f18517b1769b7e5027d76f4cf12478a14a3e
Sonny Lin <time_current@hotmail.com>
2015-04-21 18:54:41 -0700

    Added Share Feature

--- app/src/main/java/edu/uci/apperture/Start.java
+++ app/src/main/java/edu/uci/apperture/Start.java
@@ -8,7 +8,11 @@ import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import edu.uci.apperture.fragments.AboutDialogFragment;
@@ -18,7 +22,7 @@ import edu.uci.apperture.fragments.HSVColorPickerDialog;
  * Main User Start Menu
  * Created by Sonny on 4/21/2015.
  */
-public class Start extends FragmentActivity implements View.OnClickListener, HSVColorPickerDialog.OnColorSelectedListener {
+public class Start extends ActionBarActivity implements View.OnClickListener, HSVColorPickerDialog.OnColorSelectedListener {
     private static final String TAG = Start.class.getSimpleName();
     private SharedPreferences mPreferences;
     private int playerSelection = 0;
@@ -62,6 +66,37 @@ public class Start extends FragmentActivity implements View.OnClickListener, HSV
                 startActivity(new Intent(this, Main.class));
                 break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_start, menu);
+        // Get the menu item.
+        MenuItem menuItem = menu.findItem(R.id.menu_share);
+        // Get the provider and hold onto it to set/change the share intent.
+
+        ShareActionProvider mShareActionProvider = new ShareActionProvider(this);
+        MenuItemCompat.setActionProvider(menuItem, mShareActionProvider);
+
+        // Attach an intent to this ShareActionProvider.  You can update this at any time,
+        // like when the user selects a new piece of data they might like to share.
+        mShareActionProvider.setShareIntent(sharingIntent());
+
+        // This line chooses a custom shared history xml file. Omit the line if using
+        // the default share history file is desired.
+//        mShareActionProvider.setShareHistoryFileName("custom_share_history.xml");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private Intent sharingIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "Vist our Facebook/Twitter/Weebly site!\n" +
+                "Facebook: facebook.com/apperture2015\n" +
+                "Twitter: twitter.com/APPerture2015\n" +
+                "Weebly: apperture.weebly.com/about.html\n");
+
+        return intent;
     }
 
     private void showColorDialog(int initColor) {
